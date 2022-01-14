790d2249adef737b59727b5bc679707d9a140783
jcoggins <jcog306749@earthlink.net>
2022-01-14 00:02:14 -0500

    i18n - First Commit

--- src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.stacksimplify.restservices.Hello;
 
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	
 	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
 	@GetMapping("/helloworld")
@@ -16,5 +24,12 @@ public class HelloWorldController {
 	@GetMapping("/helloworld-bean")
 	public UserDetails helloWorldBean() {
 		return new UserDetails("Joe", "Coggins", "Hyattsville");
+	}
+	
+	@GetMapping("/hello-int")
+	public String getMessagesInI18NFormat(@RequestHeader(
+			name = "Accept-Language", required=false) String locale) {
+	
+		return messageSource.getMessage("label.hello", null, new Locale(locale));
 	}
 }
